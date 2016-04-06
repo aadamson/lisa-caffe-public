adb4ce538881b18a557883287bd8db7cfc382b17
aadamson <aadamson@stanford.edu>
2016-04-06 07:01:00 +0000

    Use trained goognet

--- examples/LRCN_activity_recognition/run_singleFrame_goog.sh
+++ examples/LRCN_activity_recognition/run_singleFrame_goog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 TOOLS=../../build/tools
 
-GLOG_logtostderr=1 $TOOLS/caffe train -solver solver/singleFrame_solver_goog.prototxt
+GLOG_logtostderr=1 $TOOLS/caffe train -solver solver/singleFrame_solver_goog.prototxt -weights bvlc_googlenet.caffemodel
 echo 'Done.'
