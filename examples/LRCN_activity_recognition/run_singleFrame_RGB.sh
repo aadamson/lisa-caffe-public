ec10af43986e876f1e487cdb64532ceeaa34a3a1
aadamson <alex.b.adamson@gmail.com>
2016-04-06 19:41:03 -0700

    Fix shell script for single frame RGB

--- examples/LRCN_activity_recognition/run_singleFrame_RGB.sh
+++ examples/LRCN_activity_recognition/run_singleFrame_RGB.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 TOOLS=../../build/tools
 
+export HDF5_DISABLE_VERSION_CHECK=1
+export PYTHONPATH=.
+
 GLOG_logtostderr=1 $TOOLS/caffe train -solver solver/singleFrame_solver_RGB.prototxt -weights caffe_imagenet_hyb2_wr_rc_solver_sqrt_iter_310000 
 echo 'Done.'
